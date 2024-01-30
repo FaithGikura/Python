956ef6655721a2aa545796df358bb3475c5e3b18
FaithGikura <116267236+FaithGikura@users.noreply.github.com>
2024-01-30 17:47:25 +0300

    Create Divisibility.py
    Check whether number is divisible by 5,7,11 or even

---
+++ Divisibility.py
@@ -0,0 +1,21 @@
+#calculating divisibility
+num = int(input("Enter a Number: "))
+if num % 5 == 0 or num % 7 == 0 or num % 11 == 0 or num % 2 == 0:
+    if num % 5 == 0:
+        print("The number is divisible by 5")
+    else:
+        print("Not divisible by 5")
+        if num % 7 == 0:
+            print("The number is divisible by 7")
+        else:
+            print("Not divisible by 7")
+        if num % 11 == 0:
+            print("The number is divisible by 11")
+        else:
+            print("Not divisible by 11")
+        if num % 2 == 0:
+            print("The number is even")
+        else:
+            print("Not even")
+else:
+    print("Divisible by neither")
